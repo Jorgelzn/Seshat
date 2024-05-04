b9970700593dcc629fa06d83c5f880ca35ca65a5
Jorgelzn <jorgelizgo@gmail.com>
2024-05-05 01:54:44 +0200

    init

--- Ragni_telegram.py
+++ Ragni_telegram.py
@@ -2,22 +2,15 @@ import os
 import telebot
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain_community.llms import HuggingFaceHub
 from langchain_community.llms import HuggingFaceEndpoint
-from langchain_community.chat_models.huggingface import ChatHuggingFace
-from langchain.schema import (
-    HumanMessage,
-    SystemMessage,
-)
+
 
 hf_token = os.getenv("HF_TOKEN")
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
 telegram_token = os.getenv("TELEGRAM_TOKEN")
 repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
 
-llm = HuggingFaceEndpoint(
-    repo_id=repo_id, max_length=128, temperature=0.5, token=hf_token
-)
+llm = HuggingFaceEndpoint(repo_id=repo_id,temperature=0.5)
 
 
 template = """You are an intelligent bird companion called Ragni and only answer questions about fantasy books.
@@ -29,7 +22,7 @@ Ragni:"""
 
 prompt = PromptTemplate.from_template(template)
 
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+llm_chain = prompt | llm
 
 bot = telebot.TeleBot(telegram_token)
 
@@ -38,7 +31,8 @@ bot = telebot.TeleBot(telegram_token)
 def send_welcome(message):
     msg = message.text[6:]
     name = message.from_user.first_name
-    response = llm_chain.run({"name":name,"question":msg})
+    print(name+": "+msg)
+    response = llm_chain.invoke({"name":name,"question":msg})
     bot.reply_to(message, response)
 
 
