d0e485c422434854be5ee31c8998a66c8d4b75d5
Jorgelzn <jorgelizgo@gmail.com>
2024-06-01 00:58:44 +0200

    starting with ollama-langchain

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from langchain_community.llms import Ollama
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+llm = Ollama(model="llama3")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(llm.invoke("Tell me a joke"))
