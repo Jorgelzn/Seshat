56ced28d381ec1e48dcf1df1cdc0b08709d46eac
Jorgelzn <jorgelizgo@gmail.com>
2024-06-01 11:30:38 +0200

    first agent ready

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from langchain.tools.render import render_text_description
 from langchain_community.chat_models import ChatOllama
-from langchain import hub
 from langchain.tools import tool
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents import AgentExecutor, load_tools
-from langchain.agents.output_parsers import (
-    ReActJsonSingleInputOutputParser,
-)
-
+from operator import itemgetter
+from langchain.tools.render import render_text_description
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
 
 @tool
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers."""
     return a * b
+
 
 @tool
 def add(a: int, b: int) -> int:
@@ -22,32 +19,32 @@ def add(a: int, b: int) -> int:
 
 chat_model = ChatOllama(model="llama3")
 
-tools = load_tools([add,multiply])
+tools = [add, multiply]
 
-# setup ReAct style prompt
-prompt = hub.pull("hwchase17/react-json")
-prompt = prompt.partial(
-    tools=render_text_description(tools),
-    tool_names=", ".join([t.name for t in tools]),
+
+def tool_chain(model_output):
+    print(model_output)
+    tool_map = {tool.name: tool for tool in tools}
+    chosen_tool = tool_map[model_output["name"]]
+    return itemgetter("arguments") | chosen_tool
+
+
+rendered_tools = render_text_description(tools)
+
+system_prompt = f"""You are an assistant that has access to the following set of tools. Here are the names and descriptions for each tool:
+
+{rendered_tools}
+
+Given the user input choose the tool that is more suitable for the problem to solve. Return your response as a JSON blob with 'name' and 'arguments' keys.
+Arguments must have each one their own key-value pair.
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system_prompt), ("user", "{input}")]
 )
 
-# define the agent
-chat_model_with_stop = chat_model.bind(stop=["\nObservation"])
-agent = (
-    {
-        "input": lambda x: x["input"],
-        "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
-    }
-    | prompt
-    | chat_model_with_stop
-    | ReActJsonSingleInputOutputParser()
-)
+chain = prompt | chat_model | JsonOutputParser() | tool_chain
 
-# instantiate AgentExecutor
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain_result = chain.invoke({"input": "what is the result of adding 3 and 5, and then multiplying the result by 10?"})
 
-agent_executor.invoke(
-    {
-        "input": "what is the result of adding 3 and 5, and then multiplying the result by 10?"
-    }
-)
+print(chain_result)
