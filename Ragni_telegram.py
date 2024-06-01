183c8fd63252290e19a97bdd991267e5a9a2c28e
Jorgelzn <jorgelizgo@gmail.com>
2024-06-01 16:37:20 +0200

    telegram test

--- Ragni_telegram.py
+++ Ragni_telegram.py
@@ -1,27 +1,24 @@
 import os
 import telebot
 from langchain.prompts import PromptTemplate
-from langchain_community.llms import HuggingFaceEndpoint
+from langchain_community.chat_models import ChatOllama
 
-
-hf_token = os.getenv("HF_TOKEN")
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
 telegram_token = os.getenv("TELEGRAM_TOKEN")
-repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
 
-llm = HuggingFaceEndpoint(repo_id=repo_id,temperature=0.5)
+chat_model = ChatOllama(model="llama3")
 
 
 template = """You are an intelligent bird companion called Ragni and only answer questions about fantasy books.
 If the question is not related to these themes, just answer expressing that you only know about those two themes and dont answer to that question.
-The user who is asking you is called {name}, use his or her name to answer the question.
+The user who is asking you is called {name}, use his or her name when asnwering.
+Give the answer in the same language as the question.
 User: {question}
 
 Ragni:"""
 
 prompt = PromptTemplate.from_template(template)
 
-llm_chain = prompt | llm
+llm_chain = prompt | chat_model
 
 bot = telebot.TeleBot(telegram_token)
 
@@ -32,7 +29,7 @@ def send_welcome(message):
     name = message.from_user.first_name
     print(name+": "+msg)
     response = llm_chain.invoke({"name":name,"question":msg})
-    bot.reply_to(message, response)
+    bot.reply_to(message, response.content)
 
 
 bot.infinity_polling()
